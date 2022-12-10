d67762450f86edb725cc1c8bc39b4aaa8d96ad9d
amit5511 <96247681+amit5511@users.noreply.github.com>
2022-12-10 13:08:11 +0530

    Update Projectlist.js

--- src/assets/Data/Projectlist.js
+++ src/assets/Data/Projectlist.js
@@ -1,15 +1,15 @@
 import me from '../Home/me.jpg'
  export  const projectlist = [{
     url: me,
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit 
+   text: `Lorem ipsum dolor sit amet consectetur adipisicing elit 
          Voluptate delectus reiciendis soluta minima mollitia ducimus 
          dolorum iure facere in impedit, voluptatem porro labore, nulla
           voluptatibus. Velit tempore tempora natus saepe?`,
-    certifiedby: 'ABCFJDJ',
-    demo:"demo",
-    sourcecode:'code',
-    video:'dfdjnfd',
-    tech:'csdhfbf'
+    certifiedby: 'My Protfolio',
+    demo:"https://luxury-syrniki-45ed08.netlify.app/",
+    sourcecode:'https://github.com/amit5511/portfolio_Code',
+    video:'https://drive.google.com/file/d/1fYYKQQ1cAEEQgllQHxNptSRtD-vZ-HhW/view?usp=sharing',
+    tech:' HTML5 CSS3 JavaScript React EmailJs'
 },
 {
     url: me,
